1a1064e5de18cb169d5c03b6411e9db9a6247725
sakethp82 <sakethp82@gmail.com>
2025-04-21 21:26:27 -0700

    Update settings.py

--- eventregistration/eventregistration/settings.py
+++ eventregistration/eventregistration/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'eventregistration.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'eventshowcase',
+        'USER': 'root',
+        'PASSWORD': '',  # Leave blank if using sudo mysql
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
